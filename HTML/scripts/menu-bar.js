50da613940084a137629b00a1837ace9987645e6
zukedog <zukedog18@gmail.com>
2018-05-13 16:53:50 +0930

    implement menu bar operation

--- HTML/scripts/menu-bar.js
+++ HTML/scripts/menu-bar.js
@@ -1,24 +1,33 @@
-// JavaScript Document
 var menuBar = {
 	init: function () {
 		var me = this;
 		$.getJSON("menu-bar.json", function (myJSON) {
 			me.data = myJSON;
 			me.update();
+			$(".menu-bar-item > .options1").hide();
+			$(".menu-bar-item").on("click", me.itemClick);
+		});
+
+		$(":root").on("click", function (e) {
+			if (!$(e.target).hasClass("option")) {
+				$(".menu-bar-item").not(e.target).children().hide();
+			}
 		});
 	},
-
+	itemClick: function (e) {
+		$(e.target).children(".options1").toggle();
+	},
 	update: function () {
 		$("#menu-bar").html(menuBar.process(this.data, 0));
 	},
 
 	process: function (data, index) {
 		var mainClass = "option";
-		var contentClass = "options2";
+		var contentClass = "options2 options";
 		var str = "";
 		if (index == 0) {
 			mainClass = "menu-bar-item";
-			contentClass = "options1";
+			contentClass = "options1 options";
 		}
 		for (var i = 0; i < data.length; i++) {
 			var click = "";
