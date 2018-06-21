f3f85d8367d5a66263e0c27fd40269f6f4c6ab3a
zukedog <zukedog18@gmail.com>
2018-06-21 10:27:43 +0930

    compile stuff

--- HTML/scripts/compile.js
+++ HTML/scripts/compile.js
@@ -1,1 +1,28 @@
-// JavaScript Document$.ajax({						url: "minecraft_commands.txt", dataType:"text", async: true, 			success: function(result){				result = result.split("\n");				for(var i=0; i<result.length; i++){					result[i] = result[i].split(" ");				}				console.log(result);						}});function compile(ev){	var input = $("#textarea").val().split("\n");	for (var i=0; i<input.length; i++){				//(([A-Za-z0-9_-]+)|(@+(a|e|s|p|r)+(\[+((attr)+)+\])?))(?=\s)											}}
+// JavaScript Document
+
+$.ajax({
+			
+			url: "minecraft_commands.txt", dataType:"text", async: true, 
+			success: function(result){
+				result = result.split("\n");
+				for(var i=0; i<result.length; i++){
+					result[i] = result[i].split(" ");
+				}
+				console.log(result);
+			
+			}
+});
+function compile(ev){
+	var input = $("#textarea").val().split("\n");
+	for (var i=0; i<input.length; i++){
+		
+		//(([A-Za-z0-9_-]+)|(@+(a|e|s|p|r)+(\[+((attr)+)+\])?))(?=\s)
+		console.log(input[i].match(/(((~?-?[0-9]+ ){2})(~?-?[0-9]+)|([\w/:@]+)(\[[^\]]+\])?(\{[^}]+\})?)/g));
+		
+		
+		
+		
+	}
+}
+
+
